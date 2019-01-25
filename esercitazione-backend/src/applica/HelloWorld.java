f7b6cebd6632dbef021ef00469a952c4c47b436c
davidedilecce <davidevincenzo.dilecce@gmail.com>
2019-01-25 10:14:29 +0100

    Ricezione parametri come oggetti o liste

--- esercitazione-backend/src/applica/HelloWorld.java
+++ esercitazione-backend/src/applica/HelloWorld.java
@@ -2,10 +2,12 @@ package applica;
 
 import facade.PersonFacade;
 import utils.ApplicationContextProvider;
+import viewmodel.UIPerson;
 
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import java.util.List;
 
 /**
  * Created by davidevincenzodilecce on 22/01/2019.
@@ -15,9 +17,10 @@ import javax.jws.WebService;
 public class HelloWorld {
 
     @WebMethod
-    public void savePerson(@WebParam(name = "id") Integer id,@WebParam(name = "name") String name, @WebParam(name = "surname") String surname, @WebParam(name = "age") Integer age, @WebParam(name = "fiscalCode") String fiscalCode) {
+    //public void savePerson(@WebParam(name = "id") Integer id,@WebParam(name = "name") String name, @WebParam(name = "surname") String surname, @WebParam(name = "age") Integer age, @WebParam(name = "fiscalCode") String fiscalCode) {
+    public void savePerson(@WebParam(name="data") UIPerson person, @WebParam(name = "data2") List<UIPerson> ciao) {
         PersonFacade personFacade = ApplicationContextProvider.provide().getBean(PersonFacade.class);
-        personFacade.save(id, name, surname, age, fiscalCode);
+        personFacade.save(person.getId(), person.getName(), person.getSurname(), person.getAge(), person.getFiscalCode());
     }
 
     @WebMethod
---
+++ esercitazione-backend/src/viewmodel/UIPerson.java
@@ -0,0 +1,54 @@
+package viewmodel;
+
+/**
+ * Created by davidevincenzodilecce on 25/01/2019.
+ */
+public class UIPerson {
+
+    private Integer id;
+    private String name;
+    private String surname;
+    private Integer age;
+    private String fiscalCode;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode(String fiscalCode) {
+        this.fiscalCode = fiscalCode;
+    }
+}
